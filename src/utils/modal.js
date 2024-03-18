ef5b17ba825cc6235a3d8812cfa12b80f3d4c56e
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-03-18 18:05:03 +0700

    feat: add tutorial modal

---
+++ src/utils/modal.js
@@ -0,0 +1,29 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var buttonClose = document.getElementById("close");
+
+// When the page loads, show the modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+buttonClose.onclick = function() {
+  modal.style.display = "none";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
